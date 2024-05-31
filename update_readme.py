2a1ac4b169a6a960958e9afdff47366458a32b51
Didier_Usuga <128331458+xxxDIDIERxxx@users.noreply.github.com>
2024-05-31 18:59:30 -0500

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -6,13 +6,17 @@ GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 USERNAME = 'xxxDIDIERxxx'
 
 def get_repositories(username):
-    url = f'https://api.github.com/users/{xxxDIDIERxxx}/repos'
+    url = f'https://api.github.com/users/{username}/repos'
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}'
     }
     response = requests.get(url, headers=headers)
-    repos = response.json()
-    return repos
+    if response.status_code == 200:
+        repos = response.json()
+        return repos
+    else:
+        print(f"Error al obtener los repositorios: {response.status_code}")
+        return []
 
 def update_readme(repos):
     with open('README.md', 'r') as file:
@@ -28,5 +32,8 @@ def update_readme(repos):
         file.write(updated_content)
 
 if __name__ == "__main__":
+    if not GITHUB_TOKEN:
+        raise ValueError("El token de GitHub no está configurado en las variables de entorno")
+    
     repos = get_repositories(USERNAME)
     update_readme(repos)
